6d5b256e7addca2f143e4b1deab084727a2e9656
TrihstonMadden <31551067+TrihstonMadden@users.noreply.github.com>
2018-02-06 14:54:43 -0600

    Add files via upload

--- sjh_half_of_ardiuno_project.ino
+++ sjh_half_of_ardiuno_project.ino
@@ -5,7 +5,7 @@
 
 #define PIN 6
 #define NUM_LEDS 16
-#define BRIGHTNESS 25
+#define BRIGHTNESS 15
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
 
@@ -20,7 +20,7 @@ void setup() {
 
 void loop() {
         int inches;
-
+        String inchstr;
 
         
         // send data only when you receive data:
@@ -29,26 +29,36 @@ void loop() {
       int condition=1; //conditionis int type
       if(condition==1){
       int first =Serial.read(); //check for the start of a message, noted by "R"
-      Serial.println(first);
-      if (first=="R"){
+      
+      if (first== 82){
       condition=0;
       //recieve the digits that follow the "R"
       Range[0]=Serial.read();
       Range[1]=Serial.read();
-      Range[2]=Serial.read();
      
       int i;
-      String inchstr;
+      
       //iterate through Range and concatenate the digits together
-         for ( i = 0; i < 3; i++){
-      if(Range[i] != 82 && Range[i] != NULL){
+         for ( i = 0; i < 2; i++){
+      if(Range[i] != 82 && Range[i] != 0){
         //add the digits gotten by concatenation (ex. 2+2 = 22)
-        inches = Range[i]-48;
+        inchstr = inchstr + (Range[i]-48);
         
+        
+        //Serial.println(Range[i]);      
       }
       }
+      inches = inchstr.toInt();
       Serial.println(inches);
+      if(inches >=0){
+        
       set_neopixel(inches);
+      }
+      inches = 0;
+      inchstr = "";
+      for(i = 0;i<2;i++){
+        Range[i] = 0;
+      }
         }
        }
       }
@@ -57,7 +67,10 @@ void loop() {
 void set_neopixel(int inches){
   int i;
   for(i = 0;i<inches && i<16;i++){
-    strip.setPixelColor(i, strip.Color(0,0,0,255 ));
+    strip.setPixelColor(i, strip.Color(255,0,0,5 ));
+      Serial.println("hi");
   }
+  strip.show();
+  
 }
 
